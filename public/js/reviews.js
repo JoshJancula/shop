831e20412900239e807cf7fa7045d2eaa6187771
Josh Jancula <josh@jancula.com>
2018-03-05 03:59:29 +0000

    facebook

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,29 +1,3 @@
-// Grab the articles as a json
-// function getJson() {
-//   $.getJSON("/articles", function(data) {
-     
-//     // For each one
-//     for (var i = 0; i < data.length; i++) {
-//       // Display the information on the page
-//       $("#articles").append(
-//           // build a card
-          
-//         "<div class='card blue-grey'><div class='card-content white-text'><span class='card-title'>" + data.author + "<br>" + data.date + "</span>" +
-//         "<p>" + data.summary + "</p>" +
-//         "</div>" +
-//         "<div class=card-action>" + "<a href='https://www.yellowpages.com/monroe-nc/mip/mcneills-body-shop-459617928>View Source'" + 
-//         "</div>" +
-        
-//         "</div>" +
-//         "</div>"
-        
-//       );
-//     }
-//   });
-// }
-
-// get my data
-// getJson();
 
 window.onload = function() { 
 
